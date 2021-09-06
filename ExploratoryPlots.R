74f2b1082aa6a3f7c97467c14b6848ffde80a00e
plinny <83913857+plinny@users.noreply.github.com>
2021-09-06 15:48:35 -0230

    Removed exploratory plots from the main R file so they do not run everytime.

---
+++ ExploratoryPlots.R
@@ -0,0 +1,105 @@
+# ************************************************************************
+# ExploratoryPlots.R -  creates Exploratory data plots
+# 
+# I did this so plots would not generate every time I run the code
+# ************************************************************************
+
+
+
+# ********************************************************************
+# Load the main bankruptcy-prediction.R file
+# You must choose this main R file and then choose the Excel data file
+# ********************************************************************
+source(file.choose())
+
+
+#correlation plot
+corrplot(cor(bank, use = "complete.obs"), method = "number")
+
+#histograms
+#1. EPS
+eps <- ggplot(bank, aes(x=eps)) + 
+  geom_histogram(fill="steel blue", color = "black") +
+  xlim(-15,15)
+eps
+
+#2. Liquidity
+liq <- ggplot(bank, aes(x=liquidity)) + 
+  geom_histogram(fill="steel blue", color = "black") +
+  xlim(-2,2)
+liq
+
+#3. profitability
+prof <- ggplot(bank, aes(x=profitability)) + 
+  geom_histogram(fill="steel blue", color = "black") +
+  xlim(-2,2)
+prof
+
+#4. productivity
+prod <- ggplot(bank, aes(x=productivity)) + 
+  geom_histogram(fill="steel blue", color = "black") +
+  xlim(-2,2)
+prod
+
+#5. Leverage Ratio
+lr <- ggplot(bank, aes(x=leverage_ratio)) + 
+  geom_histogram(fill="steel blue", color = "black") +
+  xlim(-2,2)
+lr
+
+#6. Asset Turnover
+at <- ggplot(bank, aes(asset_turnover)) +   
+  geom_histogram(fill="steel blue", color = "black") +
+  xlim(-0,5)
+at
+
+#7. Operational Margin
+om <- ggplot(bank, aes(operational_margin)) + 
+  geom_histogram(fill="steel blue", color = "black") +
+  xlim(-2,2)
+om
+
+#8. Return on Equity
+roe <- ggplot(bank, aes(return_on_equity)) + 
+  geom_histogram(fill="steel blue", color = "black") +
+  xlim(-2,2)
+roe
+
+#9. Market Book Ratio
+br <- ggplot(bank, aes(market_book_ratio)) + 
+  geom_histogram(fill="steel blue", color = "black") +
+  xlim(-1000,1000)
+br
+
+#10. Assets Growth
+ag <- ggplot(bank, aes(x=assets_growth)) + 
+  geom_histogram(fill="steel blue", color = "black") +
+  xlim(-2,2)
+ag
+
+#11. Sales Growth
+sg <- ggplot(bank, aes(x=sales_growth)) + 
+  geom_histogram(fill="steel blue", color = "black") +
+  xlim(-2,2)
+sg
+
+#12. Employee Growth
+eg <- ggplot(bank, aes(x=employee_growth)) + 
+  geom_histogram(fill="steel blue", color = "black") +
+  xlim(-2,2)
+eg
+
+figure1 <- ggarrange(eps, liq, prof, 
+                     prod, lr, at,
+                     om, roe, br,
+                     ag,sg,eg,
+                     nrow = 4, ncol = 3)
+figure1
+
+#for loop for histograms (doesn't work, any ideas how to make it work ? ) 
+
+for (i in 1:12){
+  plot_i <- ggplot(bank, aes(x=bank[,i]))+
+    geom_histogram()
+  plot_i
+}
--- bankruptcy-prediction.R
+++ bankruptcy-prediction.R
@@ -31,97 +31,6 @@ bank %>%
 #check for missing values
 colSums(is.na(bank))
 
-#correlation plot
-corrplot(cor(bank, use = "complete.obs"), method = "number")
-
-#histograms
-#1. EPS
-eps <- ggplot(bank, aes(x=eps)) + 
-  geom_histogram(fill="steel blue", color = "black") +
-  xlim(-15,15)
-eps
-
-#2. Liquidity
-liq <- ggplot(bank, aes(x=liquidity)) + 
-  geom_histogram(fill="steel blue", color = "black") +
-  xlim(-2,2)
-liq
-
-#3. profitability
-prof <- ggplot(bank, aes(x=profitability)) + 
-  geom_histogram(fill="steel blue", color = "black") +
-  xlim(-2,2)
-prof
-
-#4. productivity
-prod <- ggplot(bank, aes(x=productivity)) + 
-  geom_histogram(fill="steel blue", color = "black") +
-  xlim(-2,2)
-prod
-
-#5. Leverage Ratio
-lr <- ggplot(bank, aes(x=leverage_ratio)) + 
-  geom_histogram(fill="steel blue", color = "black") +
-  xlim(-2,2)
-lr
-
-#6. Asset Turnover
-at <- ggplot(bank, aes(asset_turnover)) +   
-  geom_histogram(fill="steel blue", color = "black") +
-  xlim(-0,5)
-at
-
-#7. Operational Margin
-om <- ggplot(bank, aes(operational_margin)) + 
-  geom_histogram(fill="steel blue", color = "black") +
-  xlim(-2,2)
-om
-
-#8. Return on Equity
-roe <- ggplot(bank, aes(return_on_equity)) + 
-  geom_histogram(fill="steel blue", color = "black") +
-  xlim(-2,2)
-roe
-
-#9. Market Book Ratio
-br <- ggplot(bank, aes(market_book_ratio)) + 
-  geom_histogram(fill="steel blue", color = "black") +
-  xlim(-1000,1000)
-br
-
-#10. Assets Growth
-ag <- ggplot(bank, aes(x=assets_growth)) + 
-  geom_histogram(fill="steel blue", color = "black") +
-  xlim(-2,2)
-ag
-
-#11. Sales Growth
-sg <- ggplot(bank, aes(x=sales_growth)) + 
-  geom_histogram(fill="steel blue", color = "black") +
-  xlim(-2,2)
-sg
-
-#12. Employee Growth
-eg <- ggplot(bank, aes(x=employee_growth)) + 
-  geom_histogram(fill="steel blue", color = "black") +
-  xlim(-2,2)
-eg
-
-figure1 <- ggarrange(eps, liq, prof, 
-                     prod, lr, at,
-                     om, roe, br,
-                     ag,sg,eg,
-                     nrow = 4, ncol = 3)
-figure1
-
-#for loop for histograms (doesn't work, any ideas how to make it work ? ) 
-
-for (i in 1:12){
-  plot_i <- ggplot(bank, aes(x=bank[,i]))+
-    geom_histogram()
-  plot_i
-}
-
 
 #*******************************************************
 # Delete missing row code section
@@ -130,14 +39,12 @@ for (i in 1:12){
 #*******************************************************
 
 
-
 # This cleans the entire original data set (not split)
 cleanedBank <-drop_na(bank)
   
 
 
-
-# Add an index
+# Add an index just for programming purposes (will have to be ignored for data science)
 Obs <- 1:81204
 cleanedBank$Obs <-Obs
 
@@ -148,17 +55,18 @@ cleanedBank$Obs <-Obs
 # Set seed so it can be repeated
 set.seed(3141)
 
-# Randomly sample 70% percent of the cleaned dataset
+# Randomly sample 70% percent of the cleaned data set then arrange in order
 trainBank <-sample_n(cleanedBank, floor(0.7*81204))
 trainBank <-arrange(trainBank, Obs)
 
+# The remaining data is used for the test set then arranged in order
 testBank<-anti_join(cleanedBank, trainBank)
 testBank<-arrange(testBank, Obs)
 
 
 # Checks for missing data
-md.pattern(cleanedBank)
-md.pattern(trainBank)
-md.pattern(testBank)
+# md.pattern(cleanedBank)
+# md.pattern(trainBank)
+# md.pattern(testBank)
 
 
