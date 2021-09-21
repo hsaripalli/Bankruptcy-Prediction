464b114f86dd26de1b833389083d64497b53b5ea
plinny <83913857+plinny@users.noreply.github.com>
2021-09-21 09:14:38 -0230

    Fixed SVM.

--- SVM.R
+++ SVM.R
@@ -23,11 +23,27 @@ library(e1071)
 
 
 
-# svmModel <-svm(bk ~. -Obs, data = testBank, kernel = "linear", cost = 1, scale = FALSE)
-# print(svmModel)
+svmModel <-svm(bk ~. -Obs, data = trainBank_SMOTE, kernel = "linear", cost = 1, scale = TRUE)
+print(svmModel)
+summary(svmModel)
 
-# svmModel <-svm(Species ~., data = iris, kernel = "polynomial", cost = 1, scale = FALSE)
-# print(svmModel)
+
+# Training
+
+svmPredictTrain<-predict(svmModel, trainBank_SMOTE)
+
+confusionMatrix(svmPredictTrain, as.factor(trainBank_SMOTE$bk))
+pROC::roc(trainBank_SMOTE$bk, as.numeric(svmPredictTrain), plot=TRUE, print.auc = TRUE)
+
+# Testing
+
+svmPredictTest<-predict(svmModel, testBank)
+confusionMatrix(svmPredictTest, as.factor(testBank$bk))
+
+pROC::roc(testBank$bk, as.numeric(svmPredictTest), plot=TRUE, print.auc = TRUE)
+
+#svmModel <-svm(Species ~., data = iris, kernel = "polynomial", cost = 1, scale = FALSE)
+#print(svmModel)
 
 
 # cor(testBank)
