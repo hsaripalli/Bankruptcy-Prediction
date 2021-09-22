623fc4c40399c9f788441c768e483c9f9c6d8230
jessieniles <j.niles@queensu.ca>
2021-09-22 10:09:41 +0200

    Moved trees to new R script

--- naive-bayes.R
+++ naive-bayes.R
@@ -11,7 +11,7 @@ bank_nb_test <- testBank
 bank_nb_train$bk <- as.factor(bank_nb_train$bk)
 bank_nb_test$bk <- as.factor(bank_nb_test$bk)
 
-nb_model <- naive_bayes(bk ~ ., data = bank_nb_train, usekernel=T)
+nb_model <- naive_bayes(bk ~ ., data = bank_nb_train, usekernel=F)
 plot(nb_model)
 print(nb_model)
 
@@ -21,7 +21,7 @@ nb_table <- table(nb_model_predict, bank_nb_train$bk, dnn=c("Prediction","Actual
 nb_table
 1 - sum(diag(nb_table))/sum(nb_table)
 
-nb_model_predict_test <- predict(nb_model, bank_nb_test)
+nb_model_predict_test <- predict(nb_model, bank_nb_test, type = 'class')
 head(cbind(nb_model_predict_test, bank_nb_test))
 nb_table2 <- table(nb_model_predict_test, bank_nb_test$bk, dnn=c("Prediction","Actual"))
 nb_table2
@@ -38,7 +38,7 @@ library(pROC)
 
 # NB - TPR vs FPR Plot and AUC
 
-nb_predict <- prediction(nb_model_predict_test, bank_nb_test$bk)
+nb_predict <- prediction(as.numeric(nb_model_predict_test, bank_nb_test$bk)
 perf_nb <- performance(nb_predict, "tpr", "fpr")
 plot(perf_nb, colorize = TRUE)
 
@@ -49,7 +49,6 @@ auc_nb2
 #optimal cut-off using Youden's index
 nb_model_predict_test <- as.numeric(nb_model_predict_test)
 
-pROC::coords(r, "best")
 pROC::coords(r, x = "best", input = "threshold", best.method = "youden")
 
 r<- pROC::roc(bank_nb_test$bk, nb_model_predict_test, plot=TRUE,print.auc = TRUE)
